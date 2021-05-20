6df06b558198fe5e235686c68f396ce7bddcb6a2
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-20 20:03:35 +0530

    Update Operations.java

--- src/Operations.java
+++ src/Operations.java
@@ -3,6 +3,11 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.*;
 import com.student.attedence.*;
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 
 public class Operations {
 	public static void addAttendence() {
